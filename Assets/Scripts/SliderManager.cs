5eca143732b18b307296dab5de0d020e69f2b484
John J Hayes <34247425+ChefKef@users.noreply.github.com>
2021-01-30 16:43:46 -0500

    Saved changes in scene

--- Assets/Scripts/SliderManager.cs
+++ Assets/Scripts/SliderManager.cs
@@ -7,7 +7,7 @@ public class SliderManager : MonoBehaviour
     //Public variables
     public float range; //How far to one side the platform will go.
     public float speed; //How long, in seconds it takes from the platform to go from starting position to first extreme, or 1/4 of full cycle time.
-    public bool vertical; //Platforms are by default horizontal. Setting this to true will rotate the object 90 degrees, and make it move up and down instead of side to side.
+    public bool horizontal; //Platforms are by default horizontal. Setting this to true will rotate the object 90 degrees, and make it move up and down instead of side to side.
 
     //Private variables
     private int negater;
@@ -19,7 +19,7 @@ public class SliderManager : MonoBehaviour
         delta = range / speed; //How far the platform moves in one second.
         startingPos = transform.position; //Store starting position for measurement.
 
-        if(!vertical) //Rotate platform if needed.
+        if(horizontal) //Rotate platform if needed.
         {
             transform.Rotate(0.0f, 0.0f, 90.0f, Space.World);
         }
@@ -28,7 +28,7 @@ public class SliderManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(!vertical)
+        if(horizontal)
         {
             updateHorizontal();
         }
@@ -41,7 +41,7 @@ public class SliderManager : MonoBehaviour
     private void updateHorizontal()
     {
         transform.position += new Vector3(delta * Time.deltaTime * negater, 0.0f, 0.0f);
-        if(transform.position.x > startingPos.x + delta || transform.position.x < startingPos.x - delta)
+        if(transform.position.x  + delta * Time.deltaTime * negater > startingPos.x + delta || transform.position.x + delta * Time.deltaTime * negater < startingPos.x - delta)
         {
             negater = -negater;
         }
@@ -50,7 +50,7 @@ public class SliderManager : MonoBehaviour
     private void updateVertical()
     {
         transform.position += new Vector3(0.0f, delta * Time.deltaTime * negater, 0.0f);
-        if (transform.position.y > startingPos.y + delta || transform.position.y < startingPos.y - delta)
+        if (transform.position.y + delta * Time.deltaTime * negater > startingPos.y + delta || transform.position.y  + delta * Time.deltaTime * negater < startingPos.y - delta)
         {
             negater = -negater;
         }
