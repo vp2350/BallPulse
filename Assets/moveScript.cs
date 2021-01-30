570de013a71eb745e846a2f4d78d18a69f63eabe
John J Hayes <34247425+ChefKef@users.noreply.github.com>
2021-01-30 14:27:54 -0500

    Added Ball SFX

--- Assets/moveScript.cs
+++ Assets/moveScript.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class moveScript : MonoBehaviour
 {
+    //Public variables
+    public AudioSource hitSound;
+    //Private variables
     private float waitTime = 0.5f;
     private float timer = 0.0f;
     // Start is called before the first frame update
@@ -28,6 +31,8 @@ public class moveScript : MonoBehaviour
             Vector2 direction = (Vector2)transform.position - mousePos; //get vector from position to mouse pos
             this.GetComponent<Rigidbody2D>().AddForce(direction.normalized*2*(1/(Vector2.Distance(mousePos, (Vector2)transform.position))), ForceMode2D.Impulse);
 
+            //Play sound effect
+            hitSound.Play();
         }
     }
 }
