aa8effcfeb2b5c2b5b8ba330b642204ffb7cb84a
Taode Ogden <tto5826@rit.edu>
2021-01-30 17:53:10 -0500

    Camera Follow added
    Added a script to the camera to follow the ball's movements. So far, the camera has to be centered on the ball and be smoothed out a bit.

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform ball;
+    public Transform platformZ;
+
+    // Called when the ball is intialized 
+    void Awake()
+    {
+    }
+
+    //Called on the first frame
+    void start()
+    {
+        //Centers camera on ball
+        Vector3 centerPos = Camera.main.ViewportToWorldPoint(new Vector3(ball.position.x, ball.position.x, platformZ.position.z));
+        transform.position = centerPos;
+    }
+
+    // Update is called in line with the amount of physics frames
+    void FixedUpdate()
+    {
+        //Moves camera to ball's current coordinates
+
+        /**
+         * Obtaining ball position
+         * Z-value is always set to the platform's, since it is the 'deepest' layer. 
+         * Ensures that all game objects are shown on camera. 
+         */
+        Vector3 ballPos = new Vector3(ball.position.x, ball.position.x, platformZ.position.z);
+        transform.position = ballPos;
+    }
+}
